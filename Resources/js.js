a34def4548d703b1f73c35ebf8d8d6c77c75791c
Frank McDermott <fmcdermott4@gmail.com>
2021-03-27 15:04:41 -0400

    updated APIs to call daily weather API

--- Resources/js.js
+++ Resources/js.js
@@ -28,7 +28,22 @@ function getCity(city) {
     .then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-            workWithData(data, city);
+                if (localStorage.getItem("cities") === null){
+                    var storedCities = [];
+                    storedCities[0] = city;
+                    localStorage.setItem("cities", JSON.stringify(storedCities));
+                } else {
+                    var storedCities = JSON.parse(localStorage.getItem("cities"));
+                    storedCities.unshift(city);
+                    for (i=1;i<storedCities.length;i++) {
+                        if (city === storedCities[i]){
+                            storedCities.splice(i,1);
+                        }
+                    }
+                    localStorage.setItem("cities", JSON.stringify(storedCities));
+                }
+                localCities();
+                weatherUpdate(data.city.coord.lat , data.city.coord.lon, data.city.name);
         });
         } else {
             alert('Error: ' + response.statusText);
@@ -39,39 +54,73 @@ function getCity(city) {
     });
 };
 
-
-function workWithData(cityData, city) {
-    if (localStorage.getItem("cities") === null){
-        var storedCities = [];
-        storedCities[0] = city;
-        localStorage.setItem("cities", JSON.stringify(storedCities));
-    } else {
-        var storedCities = JSON.parse(localStorage.getItem("cities"));
-        storedCities.unshift(city);
-        for (i=1;i<storedCities.length;i++) {
-            if (city === storedCities[i]){
-                storedCities.splice(i,1);
-            }
+function weatherUpdate(lat , lon , city){
+    var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=` + lat + `&lon=` + lon + `&exclude=hourly,minutely&appid=90e1750460ce4e52ef6971110d7e95e1&units=imperial`;
+    fetch(apiUrl)
+    .then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                debugger;
+                $("#cityName").empty();
+                $("#cityName").append(city);
+                var today = new Date();
+                $("#currentDate").empty();
+                $("#currentDate").append("Current Date: " + today.toLocaleDateString());
+                $("#weatherConditions").empty();
+                // $("#weatherConditions").append(`<img src="http://openweathermap.org/img/wn/` + cityData.list[0].weather[0].icon + `@2x.png" alt="current weather image">`)
+                //     $("#tempHigh").empty();
+                //     $("#tempHigh").append(`High: ` + cityData.list[0].main.temp_max + `°F`)
+                //     $("#tempLow").empty()
+                //     $("#tempLow").append(`Low: ` + cityData.list[0].main.temp_min + `°F`)
+                //     $("#humidity").empty();
+                //     $("#humidity").append(`Humidity: ` + cityData.list[0].main.humidity +`%`);
+                //     $("#windSpeed").empty();
+                //     $("#windSpeed").append(`Wind speed: ` + cityData.list[0].wind.speed +`mph`);
+            });
+        } else {
+            alert('Error: ' + response.statusText);
         }
-        localStorage.setItem("cities", JSON.stringify(storedCities));
-    }
-    debugger;
-    $("#cityName").empty();
-    $("#cityName").append(cityData.city.name);
-    var today = new Date();
-    $("#currentDate").empty();
-    $("#currentDate").append("Current Date: " + today.toLocaleDateString());
-    $("#weatherConditions").empty();
-    $("#weatherConditions").append(`<img src="http://openweathermap.org/img/wn/` + cityData.list[0].weather[0].icon + `@2x.png" alt="current weather image">`)
-    $("#tempHigh").empty();
-    $("#tempHigh").append(`High: ` + cityData.list[0].main.temp_max + `°F`)
-    $("#tempLow").empty()
-    $("#tempLow").append(`Low: ` + cityData.list[0].main.temp_min + `°F`)
-    $("#humidity").empty();
-    $("#humidity").append(`Humidity: ` + cityData.list[0].main.humidity +`%`);
-    $("#windSpeed").empty();
-    $("#windSpeed").append(`Wind speed: ` + cityData.list[0].wind.speed +`mph`);
-    localCities()
-}
+    })
+    .catch(function (error) {
+        alert('Unable to connect to OpenWeatherMap');
+    });
+};
+
+
+
+
+// function workWithData(cityData, city) {
+//     if (localStorage.getItem("cities") === null){
+//         var storedCities = [];
+//         storedCities[0] = city;
+//         localStorage.setItem("cities", JSON.stringify(storedCities));
+//     } else {
+//         var storedCities = JSON.parse(localStorage.getItem("cities"));
+//         storedCities.unshift(city);
+//         for (i=1;i<storedCities.length;i++) {
+//             if (city === storedCities[i]){
+//                 storedCities.splice(i,1);
+//             }
+//         }
+//         localStorage.setItem("cities", JSON.stringify(storedCities));
+//     }
+//     debugger;
+//     $("#cityName").empty();
+//     $("#cityName").append(cityData.city.name);
+//     var today = new Date();
+//     $("#currentDate").empty();
+//     $("#currentDate").append("Current Date: " + today.toLocaleDateString());
+//     $("#weatherConditions").empty();
+//     $("#weatherConditions").append(`<img src="http://openweathermap.org/img/wn/` + cityData.list[0].weather[0].icon + `@2x.png" alt="current weather image">`)
+//     $("#tempHigh").empty();
+//     $("#tempHigh").append(`High: ` + cityData.list[0].main.temp_max + `°F`)
+//     $("#tempLow").empty()
+//     $("#tempLow").append(`Low: ` + cityData.list[0].main.temp_min + `°F`)
+//     $("#humidity").empty();
+//     $("#humidity").append(`Humidity: ` + cityData.list[0].main.humidity +`%`);
+//     $("#windSpeed").empty();
+//     $("#windSpeed").append(`Wind speed: ` + cityData.list[0].wind.speed +`mph`);
+//     localCities()
+// }
 
 // date help https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
