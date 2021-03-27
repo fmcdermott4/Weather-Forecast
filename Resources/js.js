ababfb96c48f2f46852f3f1c988c788162084955
Frank McDermott <fmcdermott4@gmail.com>
2021-03-26 22:36:35 -0400

    api call working

--- Resources/js.js
+++ Resources/js.js
@@ -1,17 +1,30 @@
-var getUserRepos = function (user) {
-    var apiUrl = 'https://api.github.com/users/' + user + '/repos';
-  
+
+// Listening event for city submit button
+$("#citySubmit").on("click", cityInput);
+// Takes city that was inputted into form, checks for a return from the API, stores it to local memory, and updates history
+function cityInput(event){
+    event.preventDefault();
+    var city = $("#cityInput")[0].value;
+    var cityObject = getCity(city);
+}
+// api call for city weather data
+function getCity(city) {
+    var apiUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=90e1750460ce4e52ef6971110d7e95e1&limit=1'
     fetch(apiUrl)
-      .then(function (response) {
+    .then(function (response) {
         if (response.ok) {
-          response.json().then(function (data) {
-            displayRepos(data, user);
-          });
+            response.json().then(function (data) {
+            workWithData(data, city);
+        });
         } else {
-          alert('Error: ' + response.statusText);
+            alert('Error: ' + response.statusText);
         }
-      })
-      .catch(function (error) {
+    })
+    .catch(function (error) {
         alert('Unable to connect to GitHub');
-      });
-  };
+    });
+};
+
+function workWithData(cityData, city) {
+    
+}
